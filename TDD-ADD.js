3124831b84070048e89f65dc8cb7c3c69461a706
Sushil Kumar <youremail@example.com>
2025-03-05 16:20:14 +0530

    handle custom delimiters

--- TDD-ADD.js
+++ TDD-ADD.js
@@ -1,4 +1,12 @@
 function add(numbers) {
+  if (numbers.startsWith("//")) {
+    const delimiter = numbers.charAt(2);
+    numbers = numbers.slice(4);
+    return numbers
+      .split(delimiter)
+      .reduce((sum, num) => sum + parseInt(num), 0);
+  }
+
   if (numbers === "") {
     return 0;
   }
@@ -14,3 +22,5 @@ console.log(add("1") === 1); // Should return true
 console.log(add("1,5") === 6); // Should return true
 
 console.log(add("1\n2,3") === 6); // Should return true
+
+console.log(add("//;\n1;2") === 3); // Should return true
